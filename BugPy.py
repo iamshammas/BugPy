e1751c6485ac9ee3d0a593a280293659933de0a2
SHAMMAS <muhdshammas612@gmail.com>
2025-01-04 10:35:41 +0530

    live subdomains [HttpX]

--- BugPy.py
+++ BugPy.py
@@ -11,10 +11,13 @@ def subfinder(domain):
     subfinder_cmd = f"subfinder -d {domain} -all | tee -a {domain}.subs"
     return subprocess.run(subfinder_cmd, shell=True, text=True)
 
-
 def subdominator(domain):
     dm = f"subdominator -d {domain}  | tee -a {domain}.subs"
     return subprocess.run(dm, shell=True, text=True)
+
+def livechk(domain):
+    chk = f"cat {domain}.subs | httpx -o {domain}.live"
+    return subprocess.run(chk, shell=True, text=True)
 
 if tool == 1:
     print("1: Spyhunt\n2: KNOXSS")
@@ -22,12 +25,20 @@ if tool == 1:
     if autotool == 1:
         print("SPYHUNT\ngithub: https://github.com/gotr00t0day/spyhunt")
     elif autotool == 2:
+        # Subdomain Enumeration
         print('Subfinder started running....')
         time.sleep(5)
         subfinder(domain)
+        print('Subfinder completed successfully')
         print('Subdominator started running....')
         time.sleep(5)
         subdominator(domain)
+        print('Subdominator completed successfully')
+        # Check live subdomains
+        print('HttpX started running....')
+        time.sleep(5)
+        livechk(domain)
+        print('HttpX completed successfully')
 
     else:
         print("You entered wrong number")
