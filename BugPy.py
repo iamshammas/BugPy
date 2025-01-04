932ab5cf000382cd910d94cf4aebf04e4ea5edaa
SHAMMAS <muhdshammas612@gmail.com>
2025-01-04 10:07:08 +0530

    added subfinder and subdominator

--- BugPy.py
+++ BugPy.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 print("Welcome to my tool\n")
 domain = input("Enter domain url: ")
@@ -11,14 +12,23 @@ def subfinder(domain):
     return subprocess.run(subfinder_cmd, shell=True, text=True)
 
 
+def subdominator(domain):
+    dm = f"subdominator -d {domain}  | tee -a {domain}.subs"
+    return subprocess.run(dm, shell=True, text=True)
+
 if tool == 1:
     print("1: Spyhunt\n2: KNOXSS")
     autotool = int(input("Choose from the following: "))
     if autotool == 1:
         print("SPYHUNT\ngithub: https://github.com/gotr00t0day/spyhunt")
     elif autotool == 2:
-        print("KNOXSS")
+        print('Subfinder started running....')
+        time.sleep(5)
         subfinder(domain)
+        print('Subdominator started running....')
+        time.sleep(5)
+        subdominator(domain)
+
     else:
         print("You entered wrong number")
 
